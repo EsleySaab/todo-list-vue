6c56c2a2b0676a0822aea764fbaa6a37da27a273
EsleySaab <esleysantana.sa@gmail.com>
2025-04-02 16:14:01 -0300

    feat: ensure user can add todos

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,6 @@ import TodoEmpty from "./components/TodoEmpty.vue"
 import TodoFormAdd from "./components/TodoFormAdd.vue"
 import TodoItems from "./components/TodoItems.vue"
 import TodoSpinner from "./components/TodoSpinner.vue"
-import axios from "axios"
 
 export default {
   components: {
@@ -37,14 +36,9 @@ export default {
 
   created() {
     this.loading = true
-    axios
-      .get("http://localhost:3000/todos")
-      .then((response) => {
-        this.$store.commit("storeTodos", response.data)
-      })
-      .finally(() => {
-        this.loading = false
-      })
+    this.$store.dispatch("getTodos").finally(() => {
+      this.loading = false
+    })
   },
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from "vuex"
+import axios from "axios"
 
 const store = createStore({
   state: {
@@ -9,11 +10,33 @@ const store = createStore({
     storeTodos(state, data) {
       state.todos = data
     },
+    storeTodo(state, data) {
+      state.todos.push(data)
+    },
   },
 
   getters: {},
 
-  actions: {},
+  actions: {
+    getTodos({ commit }) {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          return axios.get("http://localhost:3000/todos").then((response) => {
+            commit("storeTodos", response.data)
+            resolve()
+          })
+        }, 1000)
+      })
+    },
+
+    addTodo({ commit }, data) {
+      return axios
+        .post("http://localhost:3000/todos", data)
+        .then((response) => {
+          commit("storeTodo", response.data)
+        })
+    },
+  },
 })
 
 export default store
